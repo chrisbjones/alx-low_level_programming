4d83d48906e733052ae60b5c726d34014b97c234
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-03 13:11:52 +0000

    upo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 /* Add code into entry point main */
 
-int main(void)/* Puts export text file */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	/* main - entry point to the program */
+	puts(""Programming is like building a multilingual puzzle\n");
 	return (0);
 }
