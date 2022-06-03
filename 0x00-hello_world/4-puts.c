4bcaee920ea2641253d19e3b028939bf5d908d24
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-03 12:57:46 +0000

    uplib

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Add code into entry point main */
+
+int main(void)/* Puts export text file */
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
