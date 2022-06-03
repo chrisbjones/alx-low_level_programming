533b6d95304c343c5751479f0b623b87b5067842
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-03 14:46:55 +0000

    o

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ *main - Entry point
+ *
+ *Description: function printf is exporting a text to standard
+ *
+ *Return: Always 0(success)
+ */
+
+int main(void)
+{
+        printf("Programming is like building a multilin\n");
+        return (0);
+}
