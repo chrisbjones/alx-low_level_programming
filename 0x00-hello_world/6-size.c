6b6aa66d3b8b9b8f0ca93a27d1b4d270425958d8
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-03 16:34:58 +0000

    up

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: function sizeof  is being use
+ *
+ *Return: Always 0(success)
+ */
+
+int main(void)
+{
+        Size of a char: %d byte(s)\n", sizeof(char));
+        return (0);
+
