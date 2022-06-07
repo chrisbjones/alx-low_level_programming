1c836f54ed47587677d77a267558801139cb43c6
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-07 13:24:32 -0700

    op

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
-#include <main.h>
-/*
- *main = entry point
- *Write putchar function for ch
- *return Alway 0 (SUCCESS)
- **/
 
 int main(void)
 {
-	putchar(1);
-	putchar(2);
-	putchar(3);
-	putchar(4);
-	putchar("\n");
+	char _putchar;
 
-
+	putchar(_putchar);
 	return (0);
 }
