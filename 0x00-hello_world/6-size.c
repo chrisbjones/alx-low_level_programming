004e8c9e15eee4923b93cf910bacbb1074b74a17
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-06 20:29:33 -0700

    ip

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,21 +4,18 @@
  *main - print data type size
  *
  *
- *Return: (0) if proper
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int integerType;
-	float floatType;
-	double doubleType;
-	char charType;
 
 	/* Sizeof operator evalute the size of the variable */
-	printf("Size of int: %ld bytes\n",(unsigned long) sizeof (integerType));
-	printf("size of float: %ld bytes \n",(unsigned long)sizeof (floatType));
-	printf("size of double: %ld bytes\n",(unsigned long)sizeof(doubleType));
-	printf("size of char: %ld bytes\n",(unsigned long)sizeof (charType));
+	printf("Size of int:%ld bytes\n", sizeof(int));
+	printf("size of float: %ld bytes\n", sizeof(float));
+	printf("size of long int:%ld bytes\n", sizeof(long int));
+	printf("size of char:%ld bytes\n", sizeof(char));
+	printf("size of long long int: %ld bytes\n", sizeof(long long int));
 	return (0);
 }
 
