dab58bd1ab99ba01cc798886322d199d7dad2a1f
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-07 16:10:03 -0700

    uo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+/*
+ * main - Entry Point
+ *
+ * Decription - print _putchar
+ *
+ * return - Alway 0 (SUCCESS)
+ */
+
 int main(void)
 {
-	char _putchar;
+	char text[9] = "_putchar";
+	int i = 0;
 
-	putchar(_putchar);
+	for (i = 0; i < 8; i++);
+	{
+
+		_putchar(text[1]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main = entry point
+ *
+ *defination: prints the alphabet, in lowercas
+ *
+ * return: void
+ */
+
+int print_alphabet(void)
+{
+	
+	char letter = "a' ;
+
+	while (letter <= 'z')
+	{
+		_putchar (letter);
+		letter++;
+
+	}
+	
+	_putvhar ('\n);
+
+}
+
+
