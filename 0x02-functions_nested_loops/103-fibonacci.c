72152999309bf57bdefd32af9592610253e8ad23
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-08 09:24:03 -0700

    op

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+
+/**
+ * main - print fibonacci
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	unsigned int a, b, c, d, s;
+
+	a = 1;
+	b = 2;
+	c = 3;
+	s = 2;
+	for (d = 2; d <= 32; d++)
+	{
+		if (c % 2 == 0)
+			s = s + c;
+		a = b;
+		b = c;
+		c = a + c;
+	}
+	printf("%u\n", s);
+	return (0);
+}
