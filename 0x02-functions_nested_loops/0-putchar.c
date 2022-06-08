5cb96a3025e05f970f8c95796e01ae4524cda456
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-08 01:33:30 -0700

    po

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+#include <stdio.h>
+
+/*
+ * main - Entry Point
+ *
+ * Description - print _putchar
+ *
+ * return - Alway 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	int _putchar;
+		putchar(_putchar);
+	return ('_putchar');
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+#include <stdio.h>
+
+/*
+ * main - Entry Point
+ *
+ * Description - prints the alphabet, in lowercase
+ *
+ * return - Alway 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
