1e796a01f3f0a6a4ebcf78ecf6685ab2b4311a7c
Christopher B. Jones <joneschristopher379@gmail.com>
2022-06-13 11:43:29 -0700

    pointer to an int as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter;
+ * updates the value of 'n' points to to 98
+ * return 0;
+ *
+ **/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
